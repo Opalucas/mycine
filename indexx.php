61be75ac8110278b1c9015548519e9e8b0da7ddf
opalucas <lucas_floripess360@hotmail.com>
2021-04-09 16:13:01 -0300

    Alteração nas paginas php e html (1.2)

--- indexx.php
+++ indexx.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Meu Cinema</title>
 </head>
 <body>
+    <?php require_once "menubar.php"; ?>
     <H1>Bem Vindo ao Meu Cinema</H1>
     <p>O que vai assistir dessa vez?</p>
     <p><a href="filmes.html">Filmes</a></p>
---
+++ menubar.php
@@ -0,0 +1,34 @@
+<!-- Barra de navegação -->
+<nav class="navbar navbar-dark fixed-top navbar-expand-md">
+		<div class="container">
+			<h1 class="logo">Meu cinema</h1>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link scroll-link" href="#top-content">HOME</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link scroll-link" href="#section-1">Recentes</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link scroll-link" href="#section-2">Populares</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link scroll-link" href="#section-3">Cinema</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link scroll-link" href="#section-4">Sobre</a>
+					</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+					<button class="btn btn-sm btn-outline-secondary" type="submit"><img class="pesquisa"
+							src="assets/img/icons/pesquisa.png" alt="Pesquisar"></button>
+				</form>
+			</div>
+		</div>
+	</nav>
