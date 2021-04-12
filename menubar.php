a33696858d6698a0bffdd32eef2d34ab36c7c4c2
opalucas <lucas_floripess360@hotmail.com>
2021-04-12 08:26:11 -0300

    12.04.2021

--- menubar.php
+++ menubar.php
@@ -1,3 +1,26 @@
+<!--Style menu-->
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Filmes - MEU CINEMA</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+		integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+		integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/media-queries.css">
+	<link rel="stylesheet" href="assets/css/carousel.css">
+	<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/bebas" type="text/css" />
+
+	<!-- icon -->
+	<link rel="shortcut icon" href="assets/img/icons/logo.png">
+
+</head>
 <!-- Barra de navegação -->
 <nav class="navbar navbar-dark fixed-top navbar-expand-md">
 		<div class="container">
