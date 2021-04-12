bec6c975f0dd3db32eaa7b5d95723d1b4b5bc072
opalucas <lucas_floripess360@hotmail.com>
2021-04-12 07:40:24 -0300

    12.04.2021

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-  include "index.html";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Cinema</title>
+</head>
+<body>
+    <?php include "menubar.php"; ?>
+    <H1>Bem Vindo ao Meu Cinema</H1>
+    <p>O que vai assistir dessa vez?</p>
+    <p><a href="filmes.html">Filmes</a></p>
+    <p><a href="series.html">Séries</a></p>
+</body>
+</html>
